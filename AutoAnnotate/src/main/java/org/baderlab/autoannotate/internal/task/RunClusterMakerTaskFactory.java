d595fa27cc6032796980c2edfd34c4da62125018
Mike Kucera <mikekucera@gmail.com>
2016-01-29 14:41:56 -0500

    For clusterMaker don't use immutable column. Fixes #23.

--- AutoAnnotate/src/main/java/org/baderlab/autoannotate/internal/task/RunClusterMakerTaskFactory.java
+++ AutoAnnotate/src/main/java/org/baderlab/autoannotate/internal/task/RunClusterMakerTaskFactory.java
@@ -2,6 +2,7 @@ package org.baderlab.autoannotate.internal.task;
 
 import org.baderlab.autoannotate.internal.model.ClusterAlgorithm;
 import org.cytoscape.command.CommandExecutorTaskFactory;
+import org.cytoscape.model.CyTable;
 import org.cytoscape.work.TaskFactory;
 import org.cytoscape.work.TaskIterator;
 import org.cytoscape.work.TaskObserver;
@@ -26,8 +27,6 @@ public class RunClusterMakerTaskFactory implements TaskFactory {
 	public TaskIterator createTaskIterator(TaskObserver taskObserver) {
 		String clusterCommand = getClusterCommand();
 		String networkCommand = getNetworkCommand();
-		
-//		System.out.println("clusterCommand: " + clusterCommand);
 		return commandTaskFactory.createTaskIterator(taskObserver, clusterCommand, networkCommand);
 	}
 	
@@ -36,18 +35,30 @@ public class RunClusterMakerTaskFactory implements TaskFactory {
 	 * MKTODO if the command gets any more complex then create a ClusterMakerCommandBuilder
 	 */
 	public String getClusterCommand() {
-		// clusterAttribute - the column that clusterMaker creates
 		ClusterAlgorithm alg = params.getClusterAlgorithm();
+		String columnName = getColumnName();
 		
 		if(alg.isEdgeAttributeRequired() && cutoff != null) {
-			return String.format("cluster %s clusterAttribute=\"%s\" attribute=\"%s\" edgeCutOff=\"%s\"", alg.getCommandName(), alg.getColumnName(), params.getClusterMakerEdgeAttribute(), cutoff);
+			return String.format("cluster %s clusterAttribute=\"%s\" attribute=\"%s\" edgeCutOff=\"%s\"", alg.getCommandName(), columnName, params.getClusterMakerEdgeAttribute(), cutoff);
 		}
 		else if(alg.isEdgeAttributeRequired()) {
-			return String.format("cluster %s clusterAttribute=\"%s\" attribute=\"%s\"", alg.getCommandName(), alg.getColumnName(), params.getClusterMakerEdgeAttribute());
+			return String.format("cluster %s clusterAttribute=\"%s\" attribute=\"%s\"", alg.getCommandName(), columnName, params.getClusterMakerEdgeAttribute());
 		}
 		else {
-			return String.format("cluster %s clusterAttribute=\"%s\"", alg.getCommandName(), alg.getColumnName());
+			return String.format("cluster %s clusterAttribute=\"%s\"", alg.getCommandName(), columnName);
 		}
+	}
+	
+	private String getColumnName() {
+		CyTable table = params.getNetworkView().getModel().getDefaultNodeTable();
+		final String originalName = params.getClusterAlgorithm().getColumnName();
+		
+		String name = originalName;
+		int suffix = 2;
+		while(table.getColumn(name) != null && table.getColumn(name).isImmutable()) {
+			name = originalName + "_" + (suffix++);
+		}
+		return name;
 	}
 	
 	
