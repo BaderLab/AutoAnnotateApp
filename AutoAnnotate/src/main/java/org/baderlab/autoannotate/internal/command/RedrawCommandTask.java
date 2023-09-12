665e2c3ade7f5d6812068face5dc352bfcaeaaf7
Mike Kucera <mikekucera@gmail.com>
2023-09-12 16:21:53 -0400

    Add "eventType" parameter to redraw command

--- AutoAnnotate/src/main/java/org/baderlab/autoannotate/internal/command/RedrawCommandTask.java
+++ AutoAnnotate/src/main/java/org/baderlab/autoannotate/internal/command/RedrawCommandTask.java
@@ -1,25 +1,45 @@
 package org.baderlab.autoannotate.internal.command;
 
+import org.baderlab.autoannotate.internal.model.DisplayOptions.FillType;
 import org.baderlab.autoannotate.internal.ui.render.AnnotationRenderer;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.ContainsTunables;
 import org.cytoscape.work.TaskMonitor;
+import org.cytoscape.work.Tunable;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 public class RedrawCommandTask extends AbstractTask {
 
+	@Inject private Provider<AnnotationRenderer> rendererProvider;
+	
 	@ContainsTunables @Inject
 	public NetworkContext networkContext;
 	
-	@Inject private Provider<AnnotationRenderer> rendererProvider;
+	@Tunable(description = "for internal use")
+	public String eventType;
 	
 	@Override
 	public void run(TaskMonitor tm)  {
 		var nvs = networkContext.getNetworkViewSet();
 		var asOpt = nvs.getActiveAnnotationSet();
-		rendererProvider.get().redrawAnnotations(nvs, asOpt);
+		
+		boolean redraw = true;
+		
+		if("emChartChanged".equals(eventType) && asOpt.isPresent()) {
+			var annotationSet = asOpt.get();
+			var dispOpts = annotationSet.getDisplayOptions();
+			var sigOpts  = dispOpts.getSignificanceOptions();
+			
+			// Only redraw if EM highlight is enabled
+			redraw = sigOpts.isEM() && (sigOpts.isHighlight() || dispOpts.getFillType() == FillType.SIGNIFICANT);
+		}
+		
+		if(redraw) {
+			System.out.println("AutoAnnotate redraw");
+			rendererProvider.get().redrawAnnotations(nvs, asOpt);
+		}
 	}
 	
 }
